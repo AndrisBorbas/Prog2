3fca36bbff4b69cd239fdb8d74cfbe196941a3c2
AndrisBorbas <andris.borbas@gmail.com>
2019-02-13 16:14:26 +0100

    hftest 2 done

--- hftest/f2.cpp
+++ hftest/f2.cpp
@@ -1,4 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
+#include <stdio.h>
+#include <cmath>
 #include <iomanip>
 
 char myid[] = "Borbas Andras <andris.borbas@gmail.com> F0PQGO\n";
@@ -11,13 +15,17 @@ double fn45(const double& x) {
 }
 
 double fk45(const double& x) {
-	return (0.419*pow(x, 4) - 0.838*pow(x, 3) + 53.62*pow(x, 2) + 13 * x - 45);
+	return (0.419*pow(x, 4.0) - 0.838*pow(x, 3.0) + 53.62*pow(x, 2.0) + 13.0 * x - 45);
 }
 
 int main() {
+	setbuf(stdout, NULL);
+	printf(myid);
+	printf(mytask);
 	double x;
-	cin >> x;
-	if (x > 45)cout << setprecision(4) << fn45(x);
-	else cout << setprecision(4) << fk45(x);
+	while ((cin >> x) || !EOF) {
+		if (x > 45.0)cout << fixed << setprecision(4) << fn45(x) << endl;
+		else cout << fixed << setprecision(4) << fk45(x) << endl;
+	}
 	return(0);
 }
