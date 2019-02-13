ba830241040c178ec4c00be5630705b0d2b29cd1
AndrisBorbas <andris.borbas@gmail.com>
2019-02-13 16:37:07 +0100

    hftest f4

--- hftest/f1.cpp
+++ hftest/f1.cpp
@@ -1,11 +1,13 @@
-/*#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
 
 char myid[] = "Borbas Andras <andris.borbas@gmail.com> F0PQGO\n";
 char mytask[] = "Feladat=1 ISO 8859-2\n";
 
-int main() {
+int main1() {
 	//setbuf(stdout, NULL);
 	printf(myid);
 	printf(mytask);
 	return(0);
-}*/
+}
---
+++ hftest/f4.cpp
@@ -0,0 +1,18 @@
+//	^[^#]_*(0x) ? [0 - 9] * W6. + [A - Z] + .*qqriq(A | a)
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <iomanip>
+
+char myid[] = "Borbas Andras <andris.borbas@gmail.com> F0PQGO\n";
+char mytask[] = "Feladat=4 ISO 8859-2\n";
+
+using namespace std;
+
+int main4() {
+
+	return 0;
+}
